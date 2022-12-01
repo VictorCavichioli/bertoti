94eaa69eadfe6f91c43be3fdc7e53b89905976ac
Victor Cavichioli <vivictoraraujo@hotmail.com>
2022-11-30 21:01:10 -0300

    diagramas

--- padroesDeProjeto/mvc-patterns/src/WeatherStation.java
+++ padroesDeProjeto/mvc-patterns/src/WeatherStation.java
@@ -14,12 +14,12 @@ public class WeatherStation {
 	        }
 	      }, 5000);
 	      
-	      final Timer timer2 = new Timer();
-	      timer.schedule(new TimerTask() {
-	        public void run() {
-	        	model.setMetrics(methods.printProcessorUsage(), methods.printMemoryUsage(), methods.printDiskUsage("C"));
-	        }
-	      }, 30000);
+		final Timer timer2 = new Timer();
+		timer.schedule(new TimerTask() {
+		public void run() {
+			model.setMetrics(methods.printProcessorUsage(), methods.printMemoryUsage(), methods.printDiskUsage("C"));
+		}
+		}, 30000);
 		
 	}
 	  
